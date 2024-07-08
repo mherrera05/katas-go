8ea7c358a3cf3d5d7341348517091e074224861c
Miguel <migueljherrera@gmail.com>
2024-07-08 07:09:23 -0400

    (doc): update README (test): remove initial tests

--- bowling-game/bowling_test.go
+++ bowling-game/bowling_test.go
@@ -7,23 +7,6 @@ import (
 
 var game *Bowling
 
-func TestCreateABowlingType(test *testing.T) {
-	game = new(Bowling)
-
-	var typeof interface{} = game
-	_, ok := typeof.(*Bowling)
-
-	assert.Equal(test, true, ok)
-}
-
-func TestShouldMakeARoll(test *testing.T) {
-	game = new(Bowling)
-
-	game.Roll(5)
-
-	assert.Equal(test, 1, len(game.rolls))
-}
-
 func TestShouldScore0WhenItFailsAllTheThrows(test *testing.T) {
 	game = new(Bowling)
 
